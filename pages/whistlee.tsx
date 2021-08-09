cb546073116178b00af6bf99d530f4e57659bfd3
Colby Rabideau <me@colbyr.com>
2021-08-09 17:29:32 -0400

    play/pause the song!

--- pages/whistlee.tsx
+++ pages/whistlee.tsx
@@ -1,4 +1,23 @@
+import { useEffect, useMemo, useRef } from "react";
+
 export default function Whistlee() {
+  const session = `${Math.random()}`;
+  const audioRef = useRef<HTMLAudioElement>(null);
+  const audioElement = audioRef.current;
+  const context = useMemo(
+    () => global.AudioContext && new AudioContext(),
+    [session]
+  );
+
+  useEffect(() => {
+    if (!audioElement) {
+      return;
+    }
+
+    const track = context.createMediaElementSource(audioElement);
+    track.connect(context.destination);
+  }, [context]);
+
   return (
     <div>
       <header style={{ padding: "1rem" }}>
@@ -7,21 +26,45 @@ export default function Whistlee() {
       <main
         style={{
           display: "flex",
+
           flexDirection: "column",
           justifyContent: "center",
           alignItems: "center",
         }}
       >
-        <audio controls={true} src="/song.mp3" />
+        <audio key={session} controls={true} src="/song.mp3" ref={audioRef} />
+        <button
+          onClick={() => {
+            if (context.state === "suspended") {
+              context.resume();
+            }
+
+            if (!audioElement) {
+              return;
+            }
+
+            if (audioElement.paused) {
+              audioElement.play();
+              return;
+            }
+
+            audioElement.pause();
+          }}
+        >
+          Play/Pause
+        </button>
         <p style={{ textAlign: "center" }}>
           <a href="https://freemusicarchive.org/music/Doctor_Turtle/the-mountains-dont-care-about-you/the-void-says-hi">
             The Void Says Hi
-          </a>
+          </a>{" "}
           by Doctor Turtle
           <br />
           <br />
           <a rel="license" href="https://creativecommons.org/licenses/by/4.0">
-            <img src="https://licensebuttons.net/l/by/4.0/88x31.png" />
+            <img
+              alt="Creative Commons Attribution 4.0 International"
+              src="https://licensebuttons.net/l/by/4.0/88x31.png"
+            />
           </a>
         </p>
       </main>
