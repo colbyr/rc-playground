79e36a0ee7e4e6176d4e5646f4841c10bb5157b9
Colby Rabideau <me@colbyr.com>
2021-08-09 16:03:51 -0400

    formatting

--- whistlee/pages/index.tsx
+++ whistlee/pages/index.tsx
@@ -1,5 +1,5 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
@@ -11,10 +11,8 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Whistlee
-        </h1>
-        </main>
+        <h1 className={styles.title}>Whistlee</h1>
+      </main>
     </div>
-  )
+  );
 }
