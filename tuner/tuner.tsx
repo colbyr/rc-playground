f0786f20be347817e8005299d8c82e50932f008d
Colby Rabideau <478109+colbyr@users.noreply.github.com>
2021-08-24 13:58:27 -0400

    omg it sort of works

--- tuner/tuner.tsx
+++ tuner/tuner.tsx
@@ -1,10 +1,26 @@
-const FFT_SIZE = Math.pow(2, 12);
-const PITCH_HZ = 441;
-const SMOOTHING_CONSTANT = 0.9;
+import { range, round } from "lodash";
+const FFT_SIZE = Math.pow(2, 15);
+const REF_PITCH_HZ = 441;
+const SMOOTHING_CONSTANT = 0;
+const noteNames = [
+  "c",
+  "c♯/d♭",
+  "d",
+  "d♯/e♭",
+  "e",
+  "f",
+  "f♯/g♭",
+  "g",
+  "g♯/a♭",
+  "a",
+  "a♯/b♭",
+  "b",
+];
 
 const canvas = document.getElementById("canvas") as HTMLCanvasElement;
 canvas.height = Math.round(window.innerHeight / 2);
 canvas.width = window.innerWidth;
+const note = document.getElementById("note")!;
 const startButton = document.getElementById("start") as HTMLButtonElement;
 startButton.addEventListener("click", () => {
   startListening();
@@ -27,6 +43,10 @@ function startListening() {
       const freqCount = analyser.frequencyBinCount;
       const freqMaxHz = freqSampleRateHz / 2;
       const freqStepHz = freqMaxHz / freqCount;
+      const freqRange = range(0, freqCount).map((n) => n * freqStepHz);
+      console.info({
+        freqRange,
+      });
 
       console.table([
         {
@@ -52,7 +72,7 @@ function startListening() {
         canvasContext.fillStyle = "#000";
         canvasContext.fillRect(0, 0, canvasWidth, canvasHeight);
 
-        let nextPitch = PITCH_HZ;
+        let nextPitch = REF_PITCH_HZ;
         while (nextPitch < freqMaxHz) {
           const aIndex = Math.round(nextPitch / freqStepHz);
           canvasContext.fillStyle = "#ff000075";
@@ -65,7 +85,7 @@ function startListening() {
           nextPitch = nextPitch * 2;
         }
 
-        let prevPitch = PITCH_HZ / 2;
+        let prevPitch = REF_PITCH_HZ / 2;
         while (prevPitch > 1) {
           const aIndex = Math.round(prevPitch / freqStepHz);
           canvasContext.fillStyle = "#ff000075";
@@ -98,6 +118,14 @@ function startListening() {
         const x = loudestI * columnWidth - 1;
         const y = 0;
         canvasContext.fillRect(x, y, width + 2, height);
+
+        const freq = freqRange[loudestI];
+        const c0 = REF_PITCH_HZ * Math.pow(2, -4.75);
+        const h = Math.round(12 * Math.log2(freq / c0));
+        const octave = Math.floor(h / 12);
+        const n = h % 12;
+        const noteName = noteNames[n];
+        note.innerHTML = `${h} - ${noteName}${octave}`;
 
         requestAnimationFrame(draw);
       }
