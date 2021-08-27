df387cb6979f382e9aa98d87f7a7683d08044172
Colby Rabideau <478109+colbyr@users.noreply.github.com>
2021-08-27 13:31:35 -0400

    canvas demo for presentations

--- vite.config.js
+++ vite.config.js
@@ -16,6 +16,7 @@ export default (options) => {
         input: {
           main: resolve(__dirname, "index.html"),
           a: resolve(__dirname, "a/index.html"),
+          "canvas-demo": resolve(__dirname, "canvas-demo/index.html"),
           piano: resolve(__dirname, "piano/index.html"),
           whistlee: resolve(__dirname, "whistlee/index.html"),
         },
