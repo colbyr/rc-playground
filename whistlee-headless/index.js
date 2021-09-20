7a12f7d700a15beeeeabab45c4e676c24626a8d9
Colby Rabideau <478109+colbyr@users.noreply.github.com>
2021-09-20 18:07:50 -0400

    really basic headless implementation

---
+++ whistlee-headless/index.js
@@ -0,0 +1,17 @@
+const puppeteer = require("puppeteer");
+
+(async () => {
+  const browser = await puppeteer.launch({ headless: false });
+  const context = browser.defaultBrowserContext();
+  try {
+    await context.overridePermissions("http://localhost:3000", ["microphone"]);
+  } catch (error) {
+    console.error("FAIL", error);
+    browser.close();
+  }
+  const page = await browser.newPage();
+  // page.on('console', (msg) => console.log(...msg));
+  await page.goto("http://localhost:3000/whistlee2/");
+
+  // await browser.close();
+})();
