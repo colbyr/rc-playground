b06aafd13e3a7d71899a4a1fe76ee04b0709e62b
Colby Rabideau <478109+colbyr@users.noreply.github.com>
2021-09-29 19:00:28 -0400

    use meyda in the headless version

--- whistlee-headless/whistlee-headless.ts
+++ whistlee-headless/whistlee-headless.ts
@@ -8,17 +8,17 @@ import {
   makeSampler,
   NoteName,
 } from "../octavious";
-import { interval, map } from "rxjs";
-import { setHueLightState } from "../whistlee/HueApi";
-import { quantile } from "simple-statistics";
 import { Patterns, send } from "./comms";
+import { createMeydaAnalyzer } from "meyda";
 
 const LIGHT_ID = 1;
 
 const SMOOTHING_CONSTANT = 0;
 const MIN_LOUDNESS = 32;
-const DEFAULT_FFT_SIZE = Math.pow(2, 15);
+const DEFAULT_FFT_SIZE = Math.pow(2, 11);
 const DEFAULT_SMOOTHING_CONSTANT = 0;
+const MODE_SIZE = 24;
+const MAX_SPREAD = 120;
 
 const matchers = (
   Object.entries(Patterns) as unknown as [
@@ -29,63 +29,43 @@ const matchers = (
   makeRelativeMelodyMatcher({
     pattern,
     trigger: () => send(key),
+    bufferSize: MODE_SIZE,
   })
 );
 
-const smoothNote = makeRollingMode<number>({
-  defaultValue: 0,
-  bufferSize: 24,
-});
-const referencePitch = 441;
+const referencePitch = 440;
 const toNote = new FrequencyToNoteConverter(referencePitch);
 const sampleRate = new AudioContext().sampleRate;
 const frequencyBinCount = DEFAULT_FFT_SIZE / 2;
 const frequencyByBin = getFrequenciesByBin(sampleRate, frequencyBinCount);
 
-makeSampler(interval(10), getMicrophoneSource(), (source) => {
-  const filter = new BiquadFilterNode(source.context, {
-    type: "bandpass",
-    Q: 3,
-    frequency: 1024,
-    gain: 3,
-  });
-  const bandpass = source.connect(filter);
-  const analyser = source.context.createAnalyser();
-  analyser.fftSize = DEFAULT_FFT_SIZE;
-  analyser.smoothingTimeConstant = DEFAULT_SMOOTHING_CONSTANT;
-  console.info(
-    JSON.stringify({
-      sampleRate: analyser.context.sampleRate,
-      frequencyBinCount: analyser.frequencyBinCount,
-    })
-  );
-  bandpass.connect(analyser);
-  // only sample the first 2048 frequency bins of the spectrum
-  const sample = new Uint8Array(2048);
-  return [analyser, sample];
-})
-  .pipe(
-    map((freqs: Uint8Array) => {
-      const [p99, p9999] = quantile(
-        freqs as unknown as number[],
-        [0.99, 0.9999]
-      );
-      const diff = p9999 - p99;
-      if (diff < 16 || p99 < MIN_LOUDNESS) {
-        return new Uint8Array(2048).fill(0);
+navigator.mediaDevices.getUserMedia({ audio: true }).then((micStream) => {
+  const audioContext = new AudioContext();
+  const source = audioContext.createMediaStreamSource(micStream);
+  createMeydaAnalyzer({
+    startImmediately: true,
+    sampleRate: audioContext.sampleRate,
+    audioContext,
+    source,
+    bufferSize: DEFAULT_FFT_SIZE,
+    featureExtractors: ["amplitudeSpectrum", "spectralSpread"],
+    callback: ({ amplitudeSpectrum, spectralSpread }) => {
+      if (!amplitudeSpectrum || !spectralSpread) {
+        return;
       }
-      return freqs.map((freqLevel) => {
-        return (Math.max(freqLevel - p99, 0) / diff) * 255;
-      });
-    })
-  )
-  .subscribe((sample) => {
-    const inputLoudestBin = findLoudest(sample)?.bin || null;
-    if (!inputLoudestBin) return;
-    const loudestBin = smoothNote(inputLoudestBin);
-    const currentFrequency = frequencyByBin[loudestBin];
-    const noteNumber = toNote.number(currentFrequency);
-    matchers.forEach((m) => m(noteNumber));
+
+      const loudestNote = findLoudest(amplitudeSpectrum);
+
+      const note =
+        loudestNote && spectralSpread < MAX_SPREAD
+          ? toNote.number(frequencyByBin[loudestNote.bin])
+          : null;
+
+      console.info(note);
+
+      matchers.forEach((matcher) => matcher(note));
+    },
   });
+});
 
 export {};
