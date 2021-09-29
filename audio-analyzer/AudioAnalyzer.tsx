77314dcb43db8cbfe1d3aa4d86a2616c6014dc67
Colby Rabideau <478109+colbyr@users.noreply.github.com>
2021-09-29 17:10:35 -0400

    more data

--- audio-analyzer/AudioAnalyzer.tsx
+++ audio-analyzer/AudioAnalyzer.tsx
@@ -1,13 +1,25 @@
-import { audioContext, createMeydaAnalyzer } from "meyda";
+import { createMeydaAnalyzer } from "meyda";
 import React, { useEffect, useMemo, useState } from "react";
 import Plot from "react-plotly.js";
-import { context } from "tone";
+import { zip } from "lodash";
+
 const bufferSize = 1024;
+
+function rotate2DArray(input: number[][]) {
+  return zip(...input) as number[][];
+}
 
 type Analysis = {
   amplitudeSpectrum: number[][];
-  spectralSpread: number[];
+  rms: number[];
+  spectralSpread: Array<number | null>;
 };
+
+const makeAnalysis = (): Analysis => ({
+  amplitudeSpectrum: [],
+  rms: [],
+  spectralSpread: [],
+});
 
 export const AudioAnalyzer = () => {
   const [audioFile, setAudioFile] = useState<null | File>(null);
@@ -19,24 +31,25 @@ export const AudioAnalyzer = () => {
       return;
     }
 
+    setAnalysis(null);
     const url = URL.createObjectURL(audioFile);
     const audio = new Audio(url);
     const source = audioContext.createMediaElementSource(audio);
     source.connect(audioContext.destination);
-    const result: Analysis = {
-      amplitudeSpectrum: [],
-      spectralSpread: [],
-    };
+    const result: Analysis = makeAnalysis();
 
     const analyzer = createMeydaAnalyzer({
       sampleRate: audioContext.sampleRate,
       audioContext,
       source,
       bufferSize,
-      featureExtractors: ["amplitudeSpectrum", "spectralSpread"],
+      featureExtractors: ["amplitudeSpectrum", "rms", "spectralSpread"],
       callback: (analysisFrame) => {
-        result.amplitudeSpectrum.push(analysisFrame.amplitudeSpectrum || []);
-        result.spectralSpread.push(analysisFrame.spectralSpread);
+        result.amplitudeSpectrum.push(
+          (analysisFrame.amplitudeSpectrum as unknown as number[]) || []
+        );
+        result.rms.push(analysisFrame.rms || 0);
+        result.spectralSpread.push(analysisFrame.spectralSpread || null);
       },
     });
     audioContext.resume();
@@ -49,14 +62,16 @@ export const AudioAnalyzer = () => {
     };
     audio.addEventListener("ended", onEnd);
     return () => {
+      audio.pause();
       analyzer.stop();
       audio.removeEventListener("ended", onEnd);
     };
   }, [audioContext, audioFile]);
 
-  // console.info(analysis);
+  console.info(analysis);
+  console.info(analysis && rotate2DArray(analysis.amplitudeSpectrum));
   return (
-    <div>
+    <div style={{ padding: "1rem" }}>
       <input
         type="file"
         accept="wav"
@@ -67,23 +82,30 @@ export const AudioAnalyzer = () => {
           }
         }}
       />
-      {analysis && (
-        <main>
-          <Plot
-            data={[{ y: analysis.spectralSpread }]}
-            layout={{ title: "Spectral Spread" }}
-          />
-          <Plot
-            data={[
-              {
-                z: analysis.amplitudeSpectrum,
-                type: "heatmap",
-              },
-            ]}
-            layout={{ title: "amplitudeSpectrum" }}
-          />
-        </main>
-      )}
+      <main style={{ marginTop: "1rem" }}>
+        {!analysis && audioFile && <div>analyzing {audioFile.name}...</div>}
+        {analysis && (
+          <>
+            <Plot
+              data={[{ y: analysis.rms }]}
+              layout={{ title: "Root Mean Square" }}
+            />
+            <Plot
+              data={[{ y: analysis.spectralSpread }]}
+              layout={{ title: "Spectral Spread" }}
+            />
+            <Plot
+              data={[
+                {
+                  z: rotate2DArray(analysis.amplitudeSpectrum),
+                  type: "heatmap",
+                },
+              ]}
+              layout={{ title: "Amplitude Spectrum" }}
+            />
+          </>
+        )}
+      </main>
     </div>
   );
 };
