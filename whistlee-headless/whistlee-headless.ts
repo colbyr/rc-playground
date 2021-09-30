d5c63c09c7c426d082c81de664f9b20bd17e5223
Colby Rabideau <478109+colbyr@users.noreply.github.com>
2021-09-30 15:55:18 -0400

    use avg spectral spread

--- whistlee-headless/whistlee-headless.ts
+++ whistlee-headless/whistlee-headless.ts
@@ -2,23 +2,15 @@ import {
   findLoudest,
   FrequencyToNoteConverter,
   getFrequenciesByBin,
-  getMicrophoneSource,
   makeRelativeMelodyMatcher,
-  makeRollingMode,
-  makeSampler,
+  makeRollingMean,
   NoteName,
 } from "../octavious";
 import { Patterns, send } from "./comms";
 import { createMeydaAnalyzer } from "meyda";
 
-const LIGHT_ID = 1;
-
-const SMOOTHING_CONSTANT = 0;
-const MIN_LOUDNESS = 32;
 const DEFAULT_FFT_SIZE = Math.pow(2, 11);
-const DEFAULT_SMOOTHING_CONSTANT = 0;
 const MODE_SIZE = 24;
-const MAX_SPREAD = 120;
 
 const matchers = (
   Object.entries(Patterns) as unknown as [
@@ -33,6 +25,10 @@ const matchers = (
   })
 );
 
+const getAverageSpread = makeRollingMean({
+  defaultValue: 100,
+  bufferSize: 512,
+});
 const referencePitch = 440;
 const toNote = new FrequencyToNoteConverter(referencePitch);
 const sampleRate = new AudioContext().sampleRate;
@@ -54,11 +50,12 @@ navigator.mediaDevices.getUserMedia({ audio: true }).then((micStream) => {
         return;
       }
 
-      const loudestNote = findLoudest(amplitudeSpectrum);
+      const avgSpread = getAverageSpread(spectralSpread);
+      const loudest = findLoudest(amplitudeSpectrum);
 
       const note =
-        loudestNote && spectralSpread < MAX_SPREAD
-          ? toNote.number(frequencyByBin[loudestNote.bin])
+        loudest && spectralSpread < avgSpread
+          ? toNote.number(frequencyByBin[loudest.bin])
           : null;
 
       matchers.forEach((matcher) => matcher(note));
